abaeb7730cbf4702d2878d683a8116636d27585f
noemvy <nvrm726@gmail.com>
2024-04-30 11:41:24 -0500

    editando titulo de la pantalla

--- pantallas/sacarCirculacion.php
+++ pantallas/sacarCirculacion.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anulacion Cheques</title>
+    <title>Sacar de Circulacion</title>
 </head>
 
 <body>
